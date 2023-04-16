04f90c5363b6e22680280d3dc6a090559b1db2a7
Joshua Hegedus <jhegedus9@gmail.com>
2023-04-17 01:54:44 +0200

    testing

--- bible/test.py
+++ bible/test.py
@@ -3,10 +3,17 @@ import json
 
 api_key = "baee0492ff2cb6bf8a5ff7e340d2ac92"
 
-test = requests.get('https://api.scripture.api.bible/v1/bibles', headers={'api-key': api_key})
+test = requests.get('https://api.scripture.api.bible/v1/bibles?fums-version=3', headers={'api-key': api_key})
 
-bibles = json.loads(test.content)['data']
+print(test.content)
+# bibles = json.loads(test.content)['data']
 
-for bible_data in bibles:
-    if bible_data['language']['id'] == "eng":
-        print(bible_data['name'])
+# count = len([bible for bible in bibles])
+# print(f'Bible count: {count}')
+
+# for bible_data in bibles:
+#     # if bible_data['language']['id'] == "eng":
+#     print(bible_data['name'])
+
+# print(bibles['meta']['fumsToken'])
+
