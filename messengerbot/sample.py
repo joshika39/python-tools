86628e6cb5b8eb5308be1db35781fe89d222ad49
Joshua Hegedus <jhegedus9@gmail.com>
2023-06-17 13:44:26 +0200

    new sample

--- messengerbot/sample.py
+++ messengerbot/sample.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.chrome.options import Options
-from jsonlib.json_service import JsonService 
+from jsonlib.json_service import JsonService
 import os
 import importlib
 from botlib.base import proj_root
@@ -8,13 +8,13 @@ import conversations.factory
 import sys
 import time
 smaple_msg = [
-     "Hello my friend,",
-     "",
-     "The reason that you are getting this message is that you got enrolled to Joshua's beta tester program",
-     "",
-     "He is creating a replier and messenger manager bot. Thak you for sending a message to him.",
-     "", 
-     "God bless you!" 
+    "Hello my friend,",
+    "",
+    "The reason that you are getting this message is that you got enrolled to Joshua's beta tester program",
+    "",
+    "He is creating a replier and messenger manager bot. Thak you for sending a message to him.",
+    "",
+    "God bless you!"
 ]
 
 users_path = os.path.join(proj_root(), 'users.json')
@@ -30,22 +30,25 @@ option = Options()
 option.add_argument("--disable-infobars")
 option.add_argument("start-maximized")
 option.add_argument("--disable-extensions")
-option.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2})
+option.add_experimental_option(
+    "prefs", {"profile.default_content_setting_values.notifications": 2})
 # option.set_capability("browserVersion", "99")
 
 
 browser = login(base_url, create_local(driver_options=option))
 
 while True:
-    testers = JsonService(os.path.join(proj_root(), 'lists.json')).read("beta_testers")
+    testers = JsonService(os.path.join(
+        proj_root(), 'lists.json')).read("beta_testers")
     for module in list(sys.modules.values()):
-            if "conversations" in module.__name__:
-                importlib.reload(module)
+        if "conversations" in module.__name__:
+            importlib.reload(module)
     f = conversations.factory.ConversationFactory(json_service, base_url)
     chats = f.get_open_conversations(browser)
     for chat in chats:
         print(chat.display_str())
         if chat.unread and chat.id in testers:
-             chat.reply(smaple_msg)
+            chat.reply(smaple_msg)
+            chat.archive()
     print("Refresh in 3 seconds")
     time.sleep(3)
