30b8e552ab1e2789322e0f45fc68be60000f816e
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-03-25 12:44:31 +0100

    feat: added neutralize spss code

---
+++ university/neutralize-spss.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+
+
+# This file meant to convert the string values from lime survey to numeric values for SPSS
+# e.g. "A1" -> 1, "A2" -> 2, "A3" -> 3, "A4" -> 4, "A5" -> 5
+
+def neutralize_data(file: str, overwrite: bool = False):
+    with open(file, "r") as f:
+        content = f.read()
+
+    for i in range(1, 10):
+        content = content.replace(f'"A{i}"', str(i))
+        content = content.replace(f"'A{i}'", str(i))
+
+    if overwrite:
+        with open(file, "w") as f:
+            f.write(content)
+    else:
+        name, extension = os.path.splitext(file)
+        with open(f"{name}_neutralized{extension}", "w") as f:
+            f.write(content)
+
+def neutralize_sps(file: str, overwrite: bool = False):
+    with open(file, "r") as f:
+        content = f.read()
+
+    for i in range(1, 10):
+        content = content.replace(f'"A{i}"', str(i))
+        content = content.replace(f"'A{i}'", str(i))
+
+    content = content.replace("A1", "F1")
+    content = content.replace("A2", "F2")
+
+    if overwrite:
+        with open(file, "w") as f:
+            f.write(content)
+    else:
+        name, extension = os.path.splitext(file)
+        with open(f"{name}_neutralized{extension}", "w") as f:
+            f.write(content)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Neutralize the string values from lime survey to numeric values for SPSS")
+    parser.add_argument("file", type=str, help="The file to neutralize")
+    parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite the original file")
+    args = parser.parse_args()
+
+    if args.file.endswith(".sps"):
+        neutralize_sps(args.file, args.overwrite)
+    else:
+        neutralize_data(args.file, args.overwrite)
+
+
+if __name__ == "__main__":
+    main()
