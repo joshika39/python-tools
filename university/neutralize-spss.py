53a0c466d097bc129b744f38f53361dd242187d8
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-03-29 16:37:08 +0100

    fix: using regex instead of simple text replace

--- university/neutralize-spss.py
+++ university/neutralize-spss.py
@@ -1,17 +1,27 @@
 import os
 import argparse
+import re
 
 
 # This file meant to convert the string values from lime survey to numeric values for SPSS
 # e.g. "A1" -> 1, "A2" -> 2, "A3" -> 3, "A4" -> 4, "A5" -> 5
+
+def regex_replace_word(text: str, word: str, replacement: str) -> str:
+    """
+    Replace a word in a text using regex
+
+    Replacing A2 with F2
+    Replaces A2 with F2 but not A21 with F2
+    """
+    return re.sub(rf"\b{word}\b", replacement, text)
 
 def neutralize_data(file: str, overwrite: bool = False):
     with open(file, "r") as f:
         content = f.read()
 
     for i in range(1, 10):
-        content = content.replace(f'"A{i}"', str(i))
-        content = content.replace(f"'A{i}'", str(i))
+        content = regex_replace_word(content, f"'A{i}'", str(i))
+        content = regex_replace_word(content, f'"A{i}"', str(i))
 
     if overwrite:
         with open(file, "w") as f:
@@ -26,11 +36,11 @@ def neutralize_sps(file: str, overwrite: bool = False):
         content = f.read()
 
     for i in range(1, 10):
-        content = content.replace(f'"A{i}"', str(i))
-        content = content.replace(f"'A{i}'", str(i))
+        content = regex_replace_word(content, f"'A{i}'", str(i))
+        content = regex_replace_word(content, f'"A{i}"', str(i))
 
-    content = content.replace("A1", "F1")
-    content = content.replace("A2", "F2")
+    content = regex_replace_word(content, "A1", "F1")
+    content = regex_replace_word(content, "A2", "F2")
 
     if overwrite:
         with open(file, "w") as f:
